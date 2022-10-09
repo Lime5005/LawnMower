f459b4189fc874330428c97dcb7bbe949a7cb2c0
Liping Mechling <65612959+Lime5005@users.noreply.github.com>
2022-10-09 18:51:08 +0200

    Change Lawn from class to record

--- src/main/java/model/Lawn.java
+++ src/main/java/model/Lawn.java
@@ -1,17 +1,4 @@
 package model;
 
-import lombok.*;
-
-@Getter
-@Setter
-@ToString
-@EqualsAndHashCode
-public class Lawn {
-    private final int xMax;
-    private final int yMax;
-
-    public Lawn(int xMax, int yMax) {
-        this.xMax = xMax;
-        this.yMax = yMax;
-    }
+public record Lawn(int xMax, int yMax) {
 }
