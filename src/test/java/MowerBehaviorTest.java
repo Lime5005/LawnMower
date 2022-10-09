1fd3a74ebf896b6cb853c648077021cfdbfedce9
Liping Mechling <65612959+Lime5005@users.noreply.github.com>
2022-10-09 18:52:58 +0200

    Valid mower behavior standard test

--- src/test/java/MowerBehaviorTest.java
+++ src/test/java/MowerBehaviorTest.java
@@ -1,26 +1,37 @@
 import model.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import service.MowerService;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MowerBehaviorTest {
+
+    private final MowerService mowerServiceImp = new MowerService();
+
     @Test
     public void shouldMowAsInstructions() {
         // given
-        Position position = new Position();
-        Orientation orientation = Orientation.getOrientationByOri('N');
-        Lawn lawn = new Lawn(2, 3);
+        Position position = new Position(1, 2, 'N');
+        Lawn lawn = new Lawn(5, 5);
+        List<Instruction> instructions = new ArrayList<>(); // GAGAGAGAA
+        instructions.add(Instruction.LEFT);
+        instructions.add(Instruction.FORWARD);
+        instructions.add(Instruction.LEFT);
+        instructions.add(Instruction.FORWARD);
+        instructions.add(Instruction.LEFT);
+        instructions.add(Instruction.FORWARD);
+        instructions.add(Instruction.LEFT);
+        instructions.add(Instruction.FORWARD);
+        instructions.add(Instruction.FORWARD);
         Mower mower = new Mower(lawn, position);
-        List<Instruction> instructions = new ArrayList<>();
-        mower.setInstruction(instructions);
 
         // when
-        mower.executeInstruction();
+        mowerServiceImp.executeInstruction(mower, instructions);
 
         // then
+        Position destinationPosition = new Position(1, 3, 'N');
         Assertions.assertEquals(destinationPosition, mower.getPosition());
-
     }
 }
